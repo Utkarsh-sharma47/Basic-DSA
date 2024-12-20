44c78562a8b5d0189535c5069a604b3b8476152e
UtkarshSharma <utkarsh4.sharma005@gmail.com>
2024-12-20 13:18:30 +0530

    update

--- #TEST/ICT.cpp
+++ #TEST/ICT.cpp
@@ -1,48 +1,24 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+int main ()
+{
 
+    /*basic input output*/
+    unordered_map <int , string> m;
+    m[3]="abc";
+    m[1]="bcd";
+    m[2]="zfd";
 
+    for(auto it: m)
+    {
+        cout<<it.first<<it.second<<endl;
+    }
 
-int main() {
-	// your code goes here
-   int n,k;
-	    cin>>n;
-	    vector<int> v;
-	    for(int i=0; i<n; i++)
-	    {
-	        int val;
-	        cin>>val;
-	        v.push_back(val);
-	    }
-	    v.erase(v.begin()+1);
-	    n--;
-	    for(int i=0; i<n; i++)
-	    {
-	        cout<<v[i];
-	    }
+    /*find funtion*/
+    auto x=m.find(2);
+    cout<<(*x).first;
 
-
-
-	   return 0;
+    return 0;
 }
-/*
-           for(int j=i+1; j<n; j++)//distinct element
-	        {
-	            if(v[i]+v[j]<=k)
-	            {
-	                //delete the bigger one
-	                if(v[i]>v[j])
-	                {
-	                    v.erase(v.begin()+i);
-	                }
-	                else
-	                {
-	                    v.erase(v.begin()+j);
-	                }
-	                i=-1;
-	                n=n-1;
-	                j=n;
-	            }
-	        }
-*/
 
